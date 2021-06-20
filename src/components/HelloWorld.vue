006c92e08900b9353176e1c901ca11ec253daa8d
andrewadriann <fernandodriel@gmail.com>
2021-06-20 20:14:33 +0700

    tahap3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,48 @@
 <template>
-<div>
-<div>Selamat Datang {{myText}}</div>
-<ul>
-      <li v-for="(item,idx) in todos" :key="item.id">{{idx+1}} - {{item.deskripsi}}<button @click="hapus(item.id)">X</button> </li>
-</ul>
-<input v-model="myText">
-<button @click="tambah">Add</button>
-</div>
+  <div>
+    <h1>Berikut adalah daftar tugas kita</h1>
+    <ul>
+      <li v-for="item in todos" :key="item.id">{{ item.description}}<button @click="hapus(item.id)">X</button></li>
+    </ul>
+    <input v-model="myText"/>
+    <button @click="tambah">Add</button>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-  data:function(){
-    return{
-      todos:[],
+  data: function() {
+    return {
+      todos: [],
       myText: ''
-    } 
+    }
+  },
+  created: function () {
+    axios.get('http://localhost:3000/todo')
+    .then(result => {
+      this.todos = result.data   
+    })
+  },
+  methods: {
+    tambah: function () {
+      const newItem = {description: this.myText}
+      axios.post('http://localhost:3000/todo', newItem)
+        .then((res) => {
+          this.todos.push(newItem)
+        });
+      this.myText = ''
     },
-    created: () => {
-      axios.get(http://localhost/3000/todo)
-      .then(result=>{
-        this.todos = result.data
-      })
-    },
-    methods: { 
-    tambah: function(){      
-      const newItem = {deskripsi :this.myText}
-      axios.post('http://localhost:3000/todo',newItem)
-      this.todos.push(newItem)
-      this.myText=""
-      
-    },
-    hapus:function(id){
-      axios.delete('http://localhost:3000/todo/delete/${id}').then(()=>{
-        for (var i=0;i<this.newItem.length;i++) {
-          if(this.newItem[i].id==id)
-              this.newItem.splice(i,1)
-          
-        }})
-    } 
+    hapus: function (id) {
+      axios.delete(`http://localhost:3000/todo/${id}`)
+        .then(() => {
+          for(var i = 0; i< this.todos.length; i++){
+            if(this.todos[i].id == id) {
+              this.todos.splice(i,1)
+            }
+          }
+        })
+    }
   }
 }
+</script>
